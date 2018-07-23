a23cdd391d0aabdd2950973c9f560c7bcb720a01
dawid krzus <dkrzus92@gmail.com>
2018-07-23 20:36:23 +0200

    redesign model

--- OZE.AquariumApi/Models/Response.cs
+++ OZE.AquariumApi/Models/Response.cs
@@ -2,10 +2,12 @@
 using System.Linq;
 
 namespace OZE.AquariumApi.Models {
-    public class Response<T> {
+    public class Response {
         public void AddError(string error) => Errors.Add(error);
         public ICollection<string> Errors { get; }
         public bool IsValid => Errors.Any() == false;
+    }
+    public class Response<T> :Response {
         public T Content { get; set; }
     }
 }
