abd5405de6f89908874963d4b2d65d4618cf1b3d
dawid krzus <dkrzus92@gmail.com>
2018-07-25 21:55:27 +0200

    Fix controller actions

--- OZE.AquariumApi/Controllers/AquariumController.cs
+++ OZE.AquariumApi/Controllers/AquariumController.cs
@@ -3,39 +3,39 @@ using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Mvc;
 
-using OZE.AquariumApi.HttpFactories;
 using OZE.AquariumApi.Models;
+using OZE.AquariumApi.Services;
 using OZE.AquariumApi.ViewModels;
 
 namespace OZE.AquariumApi.Controllers {
     [Route("api/aquarium")]
     [ApiController]
     public class AquariumController : ControllerBase {
-        private readonly AquariumHttpFactory aquariumHttpFactory;
+        private readonly IAquariumService aquariumService;
 
-        public AquariumController(AquariumHttpFactory aquariumHttpFactory) {
-            this.aquariumHttpFactory = aquariumHttpFactory;
+        public AquariumController(IAquariumService aquariumService) {
+            this.aquariumService = aquariumService;
         }
 
         [HttpGet]
         [Route("turnOn")]
-        public async Task<Response<StatusViewModel>> TurnOn() => await aquariumHttpFactory.TurnOn();
+        public async Task<Response<StatusViewModel>> TurnOn() => await aquariumService.TurnOn();
 
         [HttpGet]
         [Route("turnOff")]
-        public async Task<Response<StatusViewModel>> TurnOff() => await aquariumHttpFactory.TurnOff();
+        public async Task<Response<StatusViewModel>> TurnOff() => await aquariumService.TurnOff();
 
         [HttpGet]
         [Route("turnOnLedSet/{id}")]
-        public async Task<Response<StatusViewModel>> TurnOnLedSet(int id) => await aquariumHttpFactory.TurnOnLedSet(id);
+        public async Task<Response<StatusViewModel>> TurnOnLedSet(int id) => await aquariumService.TurnOnLedSet(id);
 
         [HttpGet]
         [Route("turnOffLedSet/{id}")]
-        public async Task<Response<StatusViewModel>> TurnOffLedSet(int id) => await aquariumHttpFactory.TurnOffLedSet(id);
+        public async Task<Response<StatusViewModel>> TurnOffLedSet(int id) => await aquariumService.TurnOffLedSet(id);
 
         [HttpGet]
         [Route("getLedPins")]
-        public async Task<Response<IEnumerable<int>>> GetLedIds() => await aquariumHttpFactory.GetLedPins();
+        public async Task<Response<IEnumerable<int>>> GetLedIds() => await aquariumService.GetLedPins();
 
     }
 }
