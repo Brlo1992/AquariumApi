6d1053f444db586f7c28d1b8d6033461ba3c9284
dawid krzus <dkrzus92@gmail.com>
2018-07-26 21:38:09 +0200

    Add conditional register components

--- OZE.AquariumApi/Startup.cs
+++ OZE.AquariumApi/Startup.cs
@@ -1,22 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+
 using OZE.AquariumApi.HttpFactories;
 using OZE.AquariumApi.Services;
 
-namespace OZE.AquariumApi
-{
+namespace OZE.AquariumApi {
     public class Startup
     {
+        private readonly bool deviceDisconnected = true;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -31,7 +27,11 @@ namespace OZE.AquariumApi
                 options.AddPolicy("any", builder => 
                     builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
             services.AddTransient<IDeserializeService, DeserializeService>();
-            services.AddTransient<IAquariumService, FakeAquariumService>();
+
+            if (deviceDisconnected) 
+                services.AddTransient<IAquariumService, FakeAquariumService>();
+            else
+                services.AddTransient<IAquariumService, AquariumService>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddHttpClient<AquariumHttpFactory>(client => client.BaseAddress = new Uri("http://192.168.8.133"));
