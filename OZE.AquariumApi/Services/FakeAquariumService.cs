46d210bf999e6b79e19713bddaa08015e7ec398c
dawid krzus <dkrzus92@gmail.com>
2018-07-26 21:34:05 +0200

    Implement fake service

--- OZE.AquariumApi/Services/FakeAquariumService.cs
+++ OZE.AquariumApi/Services/FakeAquariumService.cs
@@ -1,17 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using OZE.AquariumApi.HttpFactories;
 using OZE.AquariumApi.Models;
 using OZE.AquariumApi.ViewModels;
 
-namespace OZE.AquariumApi.Services
-{
+namespace OZE.AquariumApi.Services {
     public class FakeAquariumService : IAquariumService {
-        public Task<Response<IEnumerable<StatusViewModel>>> GetLedPins() => throw new NotImplementedException();
-        public Task<Response<StatusViewModel>> TurnOff() => throw new NotImplementedException();
-        public Task<Response<StatusViewModel>> TurnOffLedSet(int id) => throw new NotImplementedException();
-        public Task<Response<StatusViewModel>> TurnOn() => throw new NotImplementedException();
-        public Task<Response<StatusViewModel>> TurnOnLedSet(int id) => throw new NotImplementedException();
+
+
+        public async Task<Response<StatusViewModel>> TurnOn() {
+            return new Response<StatusViewModel> {
+                Content = new StatusViewModel {
+                    Id = 0,
+                    Status = "on"
+                }
+            };
+        }
+
+        public async Task<Response<StatusViewModel>> TurnOff() {
+            return new Response<StatusViewModel> {
+                Content = new StatusViewModel {
+                    Id = 0,
+                    Status = "off"
+                }
+            };
+        }
+
+        public async Task<Response<StatusViewModel>> TurnOnLedSet(int id) {
+            return new Response<StatusViewModel> {
+                Content = new StatusViewModel {
+                    Id = id,
+                    Status = "on"
+                }
+            };
+        }
+
+        public async Task<Response<StatusViewModel>> TurnOffLedSet(int id) {
+            return new Response<StatusViewModel> {
+                Content = new StatusViewModel {
+                    Id = id,
+                    Status = "off"
+                }
+            };
+        }
+
+        public async Task<Response<IEnumerable<StatusViewModel>>> GetLedPins() {
+            return new Response<IEnumerable<StatusViewModel>> {
+                Content = new List<StatusViewModel> {
+                    new StatusViewModel {
+                        Id = 4,
+                        Status = "off"
+                    },
+                    new StatusViewModel {
+                        Id = 5,
+                        Status = "off"
+                    },
+                    new StatusViewModel {
+                        Id = 6,
+                        Status = "off"
+                    }
+                }
+            };
+        }
     }
 }
