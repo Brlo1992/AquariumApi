a15f33d4d39736404c6fd05141d8feb6805eb5ff
dawid krzus <dkrzus92@gmail.com>
2018-07-26 21:30:34 +0200

    Add fake aquarium service implenetation

---
+++ OZE.AquariumApi/Services/FakeAquariumService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OZE.AquariumApi.Models;
+using OZE.AquariumApi.ViewModels;
+
+namespace OZE.AquariumApi.Services
+{
+    public class FakeAquariumService : IAquariumService {
+        public Task<Response<IEnumerable<StatusViewModel>>> GetLedPins() => throw new NotImplementedException();
+        public Task<Response<StatusViewModel>> TurnOff() => throw new NotImplementedException();
+        public Task<Response<StatusViewModel>> TurnOffLedSet(int id) => throw new NotImplementedException();
+        public Task<Response<StatusViewModel>> TurnOn() => throw new NotImplementedException();
+        public Task<Response<StatusViewModel>> TurnOnLedSet(int id) => throw new NotImplementedException();
+    }
+}
