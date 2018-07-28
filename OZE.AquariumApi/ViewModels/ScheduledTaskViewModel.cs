849f493ceffd4250294d867898edd1df7c2fb3d5
dawid krzus <dkrzus92@gmail.com>
2018-07-28 08:47:17 +0200

    Add view model for scheduled task

---
+++ OZE.AquariumApi/ViewModels/ScheduledTaskViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OZE.AquariumApi.ViewModels {
+    public class ScheduledTaskViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public DateTime ExecutionTime { get; set; }
+        public DateTime? LastExecutionTime { get; set; }
+    }
+}
