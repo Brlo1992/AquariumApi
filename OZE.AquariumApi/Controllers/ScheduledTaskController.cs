433211f68ff4efd927fef9cbd57d3ef976abf72d
dawid krzus <dkrzus92@gmail.com>
2018-07-28 08:45:38 +0200

    Add controller for scheduled tasks

---
+++ OZE.AquariumApi/Controllers/ScheduledTaskController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using OZE.AquariumApi.Models;
+
+namespace OZE.AquariumApi.Controllers {
+    [Route("api/scheduledTask")]
+    [ApiController]
+    public class ScheduledTaskController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<Response<ScheduledTask>> GetAllScheduledTasks() {
+
+        }
+    }
+}
