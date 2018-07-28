1f0f1285cbaca772414e5f8efc941f911793e368
dawid krzus <dkrzus92@gmail.com>
2018-07-28 17:44:36 +0200

    Throw exception from not implemented method

--- OZE.AquariumApi/Services/ScheduledTaskService.cs
+++ OZE.AquariumApi/Services/ScheduledTaskService.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using OZE.AquariumApi.Models;
 using OZE.AquariumApi.ViewModels;
@@ -6,7 +7,7 @@ using OZE.AquariumApi.ViewModels;
 namespace OZE.AquariumApi.Services {
     public class ScheduledTaskService : IScheduledTaskService {
         public Task<Response<IEnumerable<ScheduledTaskViewModel>>> GetAllAsync() {
-            
+            throw new NotImplementedException();
         }
     }
 }
