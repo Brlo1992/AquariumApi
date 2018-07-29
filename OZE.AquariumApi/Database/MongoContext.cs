1c1e1141b1384c30edb5cc72565de1869ed9aca7
dawid krzus <dkrzus92@gmail.com>
2018-07-29 11:34:09 +0200

    Add database context

---
+++ OZE.AquariumApi/Database/MongoContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OZE.AquariumApi.Database
+{
+    public class MongoContext : IDatabaseContext {
+        public MongoContext() {
+        }
+    }
+}
