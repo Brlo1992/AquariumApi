bb7d1d134075b654a9cd2e676935571c767f6455
dawid krzus <dkrzus92@gmail.com>
2018-07-30 22:19:40 +0200

    Implement interface method

--- OZE.AquariumApi/Database/MongoContext.cs
+++ OZE.AquariumApi/Database/MongoContext.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using System.Collections.Generic;
+using MongoDB.Driver;
+using OZE.AquariumApi.Models;
 
 namespace OZE.AquariumApi.Database {
     //@"mongodb://aquarium-db:uzFtIk2SWnmF2VayfTkx8i4fPt6WoyZSCfTXjbDN5MWhhVAUpaaKZpmNEoBmGPtM9WL2rckZ9qt9SE8PdYiJ6A==@aquarium-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
@@ -10,5 +12,11 @@ namespace OZE.AquariumApi.Database {
               new MongoUrl(url)
             ));
         }
+
+        public Response Add<T>(T item) => throw new System.NotImplementedException();
+        public Response<List<T>> GetAll<T>() => throw new System.NotImplementedException();
+        public Response<T> GetSingle<T>(int id) => throw new System.NotImplementedException();
+        public Response Remove(int id) => throw new System.NotImplementedException();
+        public Response Update<T>(T item) => throw new System.NotImplementedException();
     }
 }
