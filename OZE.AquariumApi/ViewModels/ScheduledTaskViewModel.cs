afbbac739a59e1a4f82a67d1eb0f1a00a1253b22
dawid krzus <dkrzus92@gmail.com>
2018-07-31 21:39:23 +0200

    Remove fake service

--- OZE.AquariumApi/ViewModels/ScheduledTaskViewModel.cs
+++ OZE.AquariumApi/ViewModels/ScheduledTaskViewModel.cs
@@ -3,6 +3,7 @@
 namespace OZE.AquariumApi.ViewModels {
     public class ScheduledTaskViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Status { get; set; }
         public DateTime ExecutionTime { get; set; }
