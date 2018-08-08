50db5b286bdc4fb93bf6de92fb5903f3df144968
dawid krzus <dkrzus92@gmail.com>
2018-08-08 23:50:31 +0200

    Change object id type in db interface

--- OZE.AquariumApi/Database/IDatabaseContext.cs
+++ OZE.AquariumApi/Database/IDatabaseContext.cs
@@ -6,9 +6,9 @@ using OZE.AquariumApi.Models;
 namespace OZE.AquariumApi.Database {
     public interface IDatabaseContext {
         Task<Response<List<T>>> GetAll<T>();
-        Task<Response<T>> GetSingle<T>(int id);
+        Task<Response<T>> GetSingle<T>(ObjectId id);
         Task<Response> Add<T>(T item);
-        Task<Response> Update<T>(T item, int id);
+        Task<Response> Update<T>(T item, ObjectId id);
         Task<Response> Remove<T>(ObjectId id);
     }
 }
